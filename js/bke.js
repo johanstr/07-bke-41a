7bd7f34c717eab2814df54c9990aa7a48f51eb01
Johan Strootman <johanstr@users.noreply.github.com>
2022-03-08 10:29:19 +0100

    Online les van 8 maart 2022
    We zijn verder gaan werken aan de functie cellClickHandler. We hebben geconstateerd dat de if-statement een enorm omvang aannam. Vandaar dat we code hebben verplaatst naar zogenaamde helper functions.

--- js/bke.js
+++ js/bke.js
@@ -46,6 +46,9 @@ let timerID = 0;
 ====================================================================================*/
 window.onload = function () {
     game_button.addEventListener('click', buttonClickHandler);
+    play_fields.forEach(cell => {
+        cell.addEventListener('click', cellClickHandler);
+    } )
 };
 
 
@@ -70,9 +73,12 @@ function buttonClickHandler(event)
 {
     if(event.target.innerHTML === 'Start ronde') {
 
+        // Rondenummer ophogen met 1
         round++;
+        // Rondenummer tonen in de UI
         element_round.innerHTML = round;
 
+        // Tekst op de button aanpassen
         event.target.innerHTML = 'Stop ronde';
 
         current_player = Math.floor((Math.random() * 2) + 1);
@@ -84,26 +90,26 @@ function buttonClickHandler(event)
 
         timerID = setInterval(gameTimer, 1000);
 
-    
-        // 5. Alle cellen klikbaar maken
         play_fields.forEach(cell => {
             cell.addEventListener('click', cellClickHandler);
             cell.src = 'img/empty.jpg';
         });
-        // 6. Speelveld leegmaken
 
     } else {
         // Onderstaande alleen uitvoeren wanneer de tekst 'Stop ronde' op de knop staat
         // 1. Speelveld leegmaken
         play_fields.forEach(cell => {
             cell.src = 'img/empty.jpg';
-            cell.removeEventListener('click', cellClickHandler);
         });
+
         // 2. Timer stoppen
         clearInterval(timerID);
 
         // 3. Cellen onklikbaar maken
-
+        play_fields.forEach(cell => {
+            cell.removeEventListener('click', cellClickHandler);  
+        });
+    
         // 4. Knoptekst veranderen in 'Start ronde'
         event.target.innerHTML = 'Start ronde';
     }
@@ -125,37 +131,54 @@ function buttonClickHandler(event)
  */
 function cellClickHandler(event)
 {
-    // 1. Symbool van speler in de cel tonen
+    // 1. Symbool van de speler in de cel tonen
     if (current_player === 1)
         event.target.src = 'img/circle.png';
     else
         event.target.src = 'img/cross.png';
     
-    // 2. De betreffende cel onklikbaar maken
+    // 2. De cel onklikbaar maken
     event.target.removeEventListener('click', cellClickHandler);
 
     // 3. Beurt wisselen
-    current_player = current_player === 1 ? 2 : 1;
+    // if (current_player === 1)
+    //     current_player = 2;
+    // else
+    //     current_player = 1;
+    
+    // De onderstaande constructie is hetzelfde als de IF-statement hierboven
+    current_player = (current_player === 1 ? 2 : 1);
 
-    // 4. Tonen wie nu aan de beurt is
+    // 4. Beurt tonen
     element_turn_playernumber.innerHTML = current_player;
     if(current_player === 1)
         element_turn_image.src = 'img/circle.png';
     else
         element_turn_image.src = 'img/cross.png';
 
-    // 5a. Controleren op een win situatie
-    if ((play_fields[0].src.includes('circle.png')
-        && play_fields[1].src.includes('circle.png')
-        && play_fields[2].src.includes('circle.png'))
-        ||
-        (play_fields[3].src.includes('circle.png')
-        && play_fields[4].src.includes('circle.png')
-        && play_fields[5].src.includes('circle.png'))
-        )
-        console.log('Speler 1 gewonnen op rij 1');
+    // 5a. Controleren of een speler gewonnen heeft
+    // console.log(event.target.src.includes('circle.png'));
+    if ( checkIfPlayerWon('circle.png') ) {
+            console.log('Speler 1 heeft gewonnen');
+            // 1. Score toekennen aan speler 1
+            // 2. Score tonen
+            // 3. Dialoogvenster tonen met de winnaar
+            // 4. Ronde stoppen
+    } else if ( checkIfPlayerWon('cross.png') ) {
+        console.log('Speler 2 heeft gewonnen');
+        // 1. Score toekennen aan speler 2
+        // 2. Score tonen
+        // 3. Dialoogvenster tonen met de winnaar
+        // 4. Ronde stoppen
+    } else if(checkForDraw()) {
+            console.log('Gelijk spel');
+            // 1. Score aan beide spelers toekennen
+            // 2. Score tonen
+            // 3. Dialoogvenster tonen m.b.t. gelijkspel
+            // 4. Ronde stoppen
+    }
 
-    // 5b. Controleren of er een gelijk spel is
+    // 5b. Daarna controleren of er een gelijk spel is
 }
 
 
@@ -174,4 +197,58 @@ function gameTimer()
 {
     round_time_in_seconds++;
     element_timer.innerHTML = round_time_in_seconds + 's';
+}
+
+
+function checkWinner(index1, index2, index3, image)
+{
+    // EN = AND = &&
+    return (
+        play_fields[index1].src.includes(image) &&                // Rij 1
+        play_fields[index2].src.includes(image) &&
+        play_fields[index3].src.includes(image)
+    );
+    /*
+        Stel: 0 = cross, 1 = circle, 2 = cross
+        play_fields[0].src.includes('cross')        Ja cross zit in cel 0, dus resultaat van deze voorwaarde is true
+        play_fields[1].src.includes('cross')        resultaat is false
+        play_fields[2].src.includes('cross')        resultaat is true
+
+        true EN false EN true => false              Bij EN moeten alle onderdelen in de voorwaarde true opleveren om het geheel een true te laten opleveren
+
+        Stel: 0 = cross, 1 = circle, 2 = circle
+        play_fields[0].src.includes('cross')        true
+        play_fields[1].src.includes('cross')        false
+        play_fields[2].src.includes('cross')        false
+
+        true OF false OF false => true              Bij OF hoeft er maar eentje true te zijn om het geheel als resultaat true op te laten leveren
+    */
+}
+
+function checkIfPlayerWon(image)
+{
+    // OF = OF = ||
+    return (
+        checkWinner(0, 1, 2, image) ||
+        checkWinner(3, 4, 5, image) ||
+        checkWinner(6, 7, 8, image) ||
+        checkWinner(0, 3, 6, image) ||
+        checkWinner(1, 4, 7, image) ||
+        checkWinner(2, 5, 8, image) ||
+        checkWinner(2, 4, 6, image) ||
+        checkWinner(0, 4, 8, image)
+    );
+}
+
+function checkForDraw()
+{
+    return (!play_fields[0].src.includes('empty.jpg') &&          // Controle op gelijk spel
+        !play_fields[1].src.includes('empty.jpg') && 
+        !play_fields[2].src.includes('empty.jpg') &&
+        !play_fields[3].src.includes('empty.jpg') && 
+        !play_fields[4].src.includes('empty.jpg') &&
+        !play_fields[5].src.includes('empty.jpg') && 
+        !play_fields[6].src.includes('empty.jpg') &&
+        !play_fields[7].src.includes('empty.jpg') && 
+        !play_fields[8].src.includes('empty.jpg'));
 }
